a9ab81f80fd56439a4a855c26c31a97b42ec7281
XiaoyaoWu <swylmiss@163.com>
2025-04-15 11:47:53 +0800

    定义视图函数

--- bookmanger/book/views.py
+++ bookmanger/book/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+视图所谓的视团 其实就是python函数
+视图函数有2个要求:
+    1. 视图函数的第一个参数就是接收请求,这个请求其实就是 HttpRequest的类对象
+"""
+
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('逍遥踏千秋')
+
+
